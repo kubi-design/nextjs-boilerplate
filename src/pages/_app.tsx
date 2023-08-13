659a15ac68fb317214a143f1ce63981657fded01
bytasv <vytautas.butkus@gmail.com>
2023-08-13 19:00:08 +0300

    Add logic to resolve providers after code sync kicks in

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,13 @@
 import '@root/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { getProviders } from '@root/utils'
+
+const Providers = getProviders()
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Providers>
+      <Component {...pageProps} />
+    </Providers>
+  )
 }
---
+++ src/utils/index.tsx
@@ -0,0 +1,36 @@
+/* eslint-disable react/display-name, react/prop-types */
+
+import React, { FC, PropsWithChildren } from 'react'
+import * as Components from '@root/components'
+import { providers } from '@root/components/providers'
+
+type ComponentName = keyof typeof Components
+
+type Providers = FC<PropsWithChildren<any>>[]
+
+const combineProviders = (providers: Providers): FC<PropsWithChildren> =>
+  providers.reduce(
+    (AccumulatedProviders, Provider) =>
+      function ({ children }) {
+        return (
+          <AccumulatedProviders>
+            <Provider>{children}</Provider>
+          </AccumulatedProviders>
+        )
+      },
+    ({ children }) => children
+  )
+
+export const getProviders = () => {
+  const Providers = providers.map((provider) => {
+    const Provider = Components[provider as ComponentName]
+
+    if (!Provider) {
+      throw new Error(`Provider ${provider} not found`)
+    }
+
+    return Provider
+  })
+
+  return combineProviders(Providers)
+}
