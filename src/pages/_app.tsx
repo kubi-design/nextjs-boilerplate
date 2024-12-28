e2ef4d6889fb121c0fedb06369c47fb1b5e53da8
bytasv <vytautas.butkus@gmail.com>
2024-12-28 23:48:20 +0200

    Tweak boilerplate

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,20 @@
 import '@root/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { getProviders } from '@root/utils'
+import { SWRConfig } from 'swr'
 
 const Providers = getProviders()
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Providers>
-      <Component {...pageProps} />
-    </Providers>
+    <SWRConfig
+      value={{
+        fetcher: (url: string) => fetch(url).then(res => res.json()),
+      }}
+    >
+      <Providers>
+        <Component {...pageProps} />
+      </Providers>
+    </SWRConfig>
   )
 }
